a3f64bfab0183e62e9518259abfab9bd76d76710
Marco <marco_trabajo@yahoo.com>
2022-01-20 13:10:06 +0100

    React JS - marco updates an array and creates todo list on click

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,45 @@
-import './App.css';
-import Header from '../Header/Header';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchHistory from '../SearchHistory/SearchHistory';
+import React from 'react';
 import { useState } from 'react'
 
+
+
 export default function App() {
-  const name = 'Company Name';
-  const [terms, setTerms] = useState([]);
+  const [term, setTerm] = useState("");
+  const [terms, setTerms] = useState(["term 1", "term 2"]);
+
+
+  function submitted(ev) {
+    ev.preventDefault();
+    // setTerm(ev.target['newSearchItem']);
+    setTerms([...terms, term]);
+  }
+  console.log(terms);
+
+
+
+
+
 
   return (
-    <div className="App">
-      <Header company={name} />
-      <SearchBar term={"default"} />
-      <SearchHistory term={terms} />
-    </div>
-  );
+    <div>
+      <h1>This is App Component</h1>
+
+
+      <div>
+      <form onSubmit={submitted}  >
+        <input type="text" name="newSearchItem" onInput={ (e) => setTerm(e.target.value)} />
+        <button type="submit"> send form </button>
+      </form>
+     <p>You searched for: {term} </p>
+      </div>
+
+
+      <ul>
+        {terms.map((eachterm) => (
+          <li>{eachterm}</li>   
+        ))}
+      </ul>
+      </div>
+
+  )
 }
